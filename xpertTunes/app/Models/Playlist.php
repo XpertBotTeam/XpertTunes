3b8395f4d300bde51cc8d3236f74250f3162bfd7
Hadi Aldhaywi <aldhaywihad@gmail.com>
2024-03-25 00:31:44 +0200

    adding functions to paylist model

--- xpertTunes/app/Models/Playlist.php
+++ xpertTunes/app/Models/Playlist.php
@@ -13,5 +13,11 @@ class Playlist extends Model
         'title',
         'user_id',
     ];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 
+    public function songs(){
+        return $this->belongsToMany(Song::class);
+    }
 }
